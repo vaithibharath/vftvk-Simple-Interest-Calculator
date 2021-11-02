288ce9fb7aac00e34a140a40427d92e711f151e5
vaithibharath <vaithis@gmail.com>
2021-11-01 23:42:36 -0400

    enhanced styling

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    //alert("compute");
-    //var retval = "computed";
-    //return retval;
-    document.write("computed")
+    let p = document.getElementById("principal").value;
+    
+    let i = document.getElementById("sliderRate").value;
+    
+    let t = parseInt(document.getElementById("years").value);
+    const d = new Date();
+    let year = d.getFullYear();
+    let maturityYear = year + t;
+
+    let r = ((p * i) / 100) * t;
+    
+    let retVal = "If you deposit " + '<mark>'+p+'</mark>' + " at an interest rate of " + '<mark>'+i+'%'+'</mark>' + ", you will receive an amount of " + '<mark>'+r+'</mark>' + " in the year " + '<mark>'+maturityYear+'</mark>'+'.';
+    document.getElementById("computedInterest").innerHTML = retVal;
+}
+
+function updateSlider()
+{
+    var display = document.getElementById("rate").value;
+    document.getElementById("sliderRate").value = display;
 }
         
